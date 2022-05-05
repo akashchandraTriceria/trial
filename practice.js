079aea9d84b6ac32a2bf8556e3d149395ed82e93
AKASH <akash.triceria@gmail.com>
2022-05-05 22:21:53 +0530

    initial

---
+++ practice.js
@@ -0,0 +1,22 @@
+console.log("my name is akash")
+// let a = [2, 3, 4, 5, 6]
+// console.log(a)
+
+// function add(a, b){
+//     console.log(a+b)
+// }
+// add(13, 17);
+
+// var add_two = function(a, b){
+//     console.log(a+b)
+// }
+// add_two(5, 10);
+
+//Array fuctions:
+// let arr = ["mango", "apple", "banana", "kiwi"];
+// console.log(arr)
+// console.log(arr.length);
+// console.log(arr.push("pineapple")); //Add element to end of array
+// console.log(arr.unshift("pineapple")); //Add element to start of array
+// console.log(arr.pop()); //Remove element from last of array
+// console.log(arr.shift()); //Remove element from start of array
